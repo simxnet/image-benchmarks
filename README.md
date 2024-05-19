32785ff03b04add330b04e15b33f199f0dc677ce
simxnet <simxnet@envs.net>
2024-05-19 19:19:28 +0200

    feat(): benchmarks

---
+++ README.md
@@ -0,0 +1,23 @@
+# benchmarks
+## Simple benchmarks using Bun
+
+> Note: `result.json` is the result of the last benchmark
+
+# As I know:
+- Sharp: The fastest
+- Imagescript: Meh
+- Jimp (compact version): oh god it's terrible (but tinier than others)
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+This project was created using `bun init` in bun v1.1.6. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.ts
@@ -0,0 +1,34 @@
+import {run, bench, group, baseline} from 'mitata';
+import Jimp from "jimp-compact";
+import { Image } from "imagescript";
+import Sharp from "sharp";
+
+
+group("1000x1000 (black) (png)", () => {
+    bench("Black image 1000x1000 (jimp-compact)",
+        () => Jimp.read(1000, 1000, 0x000000ff))
+    bench("Black image 1000x1000 (imagescript)",
+        () => new Image(1000, 1000).fill(0x000000ff))
+    bench("Black image 1000x1000 (sharp)",
+        () => Sharp().clone().resize(1000, 1000).tint("#000000"))
+})
+
+group("2000x2000 (black) (png)", () => {
+    bench("Black image 2000x2000 (jimp-compact)",
+        () => Jimp.read(2000, 2000, 0x00000ff))
+    bench("Black image 2000x2000 (imagescript)",
+        () => new Image(2000, 2000).fill(0x00000ff))
+    bench("Black image 2000x2000 (sharp)",
+        () => Sharp().clone().resize(2000, 2000).tint("#000000"))
+})
+
+run({ json: true, silent: true }).then(o => Bun.write("./result.json", Buffer.from(JSON.stringify(o.benchmarks), "utf-8")))
+
+await run({
+    silent: false, // enable/disable stdout output
+    avg: true, // enable/disable avg column (default: true)
+    json: false, // enable/disable json output (default: false)
+    colors: true, // enable/disable colors (default: true)
+    min_max: true, // enable/disable min/max column (default: true)
+    percentiles: false, // enable/disable percentiles column (default: true)
+});
